030f431c0c559d358cf5948757401233bfce3bf7
Luiz Aveiro <aveirolc@gmail.com>
2026-05-29 16:36:56 -0300

    Adicionado web scraping com requests e BeautifulSoup

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Projeto WebScrapering iniciado")
+import requests
+from bs4 import BeautifulSoup
+
+resposta = requests.get("https://example.com")
+
+soup = BeautifulSoup (resposta.text, "html.parser")
+
+print(soup.h1.text)
