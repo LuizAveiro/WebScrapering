ae4f64ad73128c301a25fbc2bfdc22e8654e0c3e
Luiz Aveiro <aveirolc@gmail.com>
2026-06-30 19:33:53 -0300

    Melhora o scraper e atualiza o código principal

--- main.py
+++ main.py
@@ -1,8 +1,45 @@
 import requests
 from bs4 import BeautifulSoup
 
+# Acessa o site
 resposta = requests.get("https://example.com")
 
-soup = BeautifulSoup (resposta.text, "html.parser")
+# Verifica se funcionou
+print("STATUS:")
+print(resposta)
 
-print(soup.h1.text)
+print("-" * 30)
+
+# Analisa o HTML
+soup = BeautifulSoup(resposta.text, "html.parser")
+
+# Título
+print("TITLE:")
+print(soup.title.text)
+
+print("-" * 30)
+
+# H1
+titulo = soup.find("h1")
+
+print("H1:")
+print(titulo.text)
+
+print("-" * 30)
+
+# Parágrafo
+paragrafos = soup.find_all("p")
+print("PARÁGRAFO:")
+print(paragrafos)
+
+print("-" * 30)
+
+for paragrafo in paragrafos:
+    print(paragrafo)
+    
+print("-" * 30)
+
+link = soup.find("a")
+print(link.text)
+
+print(link["href"])
